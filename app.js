ae772e00bfa59f1caa928cb84f8be667d2643c01
Herpryth <musicboymc1@gmail.com>
2020-12-25 09:57:44 +0200

    changes

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 app.get("/", function (req, res) {
   res.send("IT WORKED");
 });
+app.get("/new", function (req, res) {
+  res.send("New also worked");
+});
 
 app.listen(process.env.PORT || 5000);
 module.exports = app;
